6f03b6731bdfac12316524322cdfb863eab93dd0
gs-traavi <traavi@gainsight.com>
2014-12-23 16:09:51 +0530

    assignment is committed

---
+++ src/basics/assignmentproblem1.java
@@ -0,0 +1,25 @@
+package basics;
+import java.util.Scanner;
+public class assignmentproblem1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         int count = 0;
+		 Scanner scan = new Scanner(System.in);
+		 System.out.println("Enter  two Strings");
+	     String a = scan.nextLine();
+         String b = scan.nextLine();		
+		for(int i=0;(i<=a.length()-2&&i<=b.length()-2);i++)
+		{
+			String str1 = a.substring(i,i+2);
+			String str2 = b.substring(i,i+2);
+			System.out.println(str1);
+			System.out.println(str2);
+			if((str1.equals(str2))==true)
+			count++;
+		}
+		System.out.println("if a = "+a+" and  b = "+b+" The answer is "+count);
+		scan.close();
+	}
+
+}
---
+++ src/basics/assignmentproblem2.java
@@ -0,0 +1,39 @@
+package basics;
+import java.util.Scanner;
+
+public class assignmentproblem2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner scan = new Scanner(System.in);
+		 System.out.println("enter exam key for four questions");
+	        char[] examkey=scan.next().toCharArray();
+	          System.out.println("enter Answer key for four questions");
+	        char[] answers=scan.next().toCharArray();
+	        System.out.println("Exam Key is : ");
+	        for (int i=0;i<examkey.length;i++)
+	            System.out.print(examkey[i]);
+	        System.out.println();
+	        System.out.println("Answers are : ");
+	        for (int i=0;i<answers.length;i++)
+	            System.out.print(answers[i]);
+	        int marks =0;
+	        for(int i = 0;i<=3;i++)
+	        {
+	        	if(examkey[i]==answers[i])
+	        	marks+=4;
+	        		else
+	        	  marks+=-1;
+	        }
+	        System.out.println();
+	        System.out.println("Scored Marks are:-");
+	        System.out.println(marks);
+	
+	
+	scan.close();
+	}
+	
+	
+	
+
+}
